3995dded12709cd46bdb40ea47bc93eed06c2706
jnognet <jnognet@gmail.com>
2023-08-27 01:08:07 -0500

    fix mv sources

--- scripts/script_010_3.1_download_mirror_packages.sh
+++ scripts/script_010_3.1_download_mirror_packages.sh
@@ -2,7 +2,7 @@
 rm -f $LFS/sources/*
 rm -rf lfs.gnlug.org
 wget -l 1 -r https://lfs.gnlug.org/pub/lfs/lfs-packages/11.3/
-mv https://lfs.gnlug.org/pub/lfs/lfs-packages/11.3/*.bz2 $LFS/sources
-mv https://lfs.gnlug.org/pub/lfs/lfs-packages/11.3/*.gz $LFS/sources
-mv https://lfs.gnlug.org/pub/lfs/lfs-packages/11.3/*.xz $LFS/sources
-mv https://lfs.gnlug.org/pub/lfs/lfs-packages/11.3/*.patch $LFS/sources
+mv lfs.gnlug.org/pub/lfs/lfs-packages/11.3/*.bz2 $LFS/sources
+mv lfs.gnlug.org/pub/lfs/lfs-packages/11.3/*.gz $LFS/sources
+mv lfs.gnlug.org/pub/lfs/lfs-packages/11.3/*.xz $LFS/sources
+mv lfs.gnlug.org/pub/lfs/lfs-packages/11.3/*.patch $LFS/sources
