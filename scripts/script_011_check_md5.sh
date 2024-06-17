fac53928a11249aba639000efd56bd42ddd0dfba
jnognet <jnognet@gmail.com>
2024-06-16 19:50:17 -0500

    Update script_011_check_md5.sh
    fix version

--- scripts/script_011_check_md5.sh
+++ scripts/script_011_check_md5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f $LFS/sources/md5sums
-cd $LFS/sources && wget https://www.linuxfromscratch.org/lfs/view/stable/md5sums
+cd $LFS/sources && wget https://www.linuxfromscratch.org/lfs/view/11.3/md5sums
 pushd $LFS/sources
   md5sum -c md5sums
 popd
